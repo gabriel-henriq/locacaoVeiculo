53e95c2a41b7eeb3d74b80af71fd287f7740e985
Gabriel Henrique <gabrielhcm01@gmail.com>
2020-11-17 17:01:40 -0300

    Arrumando a posicao da tabela ao imprimir na tela.

--- imprimirListaDeCarros.c
+++ imprimirListaDeCarros.c
@@ -1,25 +1,25 @@
 void imprimirListaDeCarros(void) {	
 	int i = 0;
-	gotoxy(20,5);
+	gotoxy(20,4);
 	printf("ID");
-	gotoxy(25,5);
+	gotoxy(25,4);
 	printf("MARCA");
-	gotoxy(40,5);
+	gotoxy(40,4);
 	printf("MODELO");
-	gotoxy(53,5);
+	gotoxy(53,4);
 	printf("COR");
-	gotoxy(65,5);
+	gotoxy(65,4);
 	printf("ANO");
     for(i = 0; i < NUM_ID; i++) {
-    	gotoxy(20,i+7);
+    	gotoxy(20,i+6);
     	printf("%d", nCarro[i].carroID);
-    	gotoxy(25,i+7);
+    	gotoxy(25,i+6);
     	printf("%s", nCarro[i].marca);
-    	gotoxy(40,i+7);
+    	gotoxy(40,i+6);
     	printf("%s", nCarro[i].modelo);
-    	gotoxy(53,i+7);
+    	gotoxy(53,i+6);
     	printf("%s", nCarro[i].cor);
-    	gotoxy(65,i+7);
+    	gotoxy(65,i+6);
     	printf("%d", nCarro[i].ano);
 	}
 }
