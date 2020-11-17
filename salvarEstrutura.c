9605609b133578646b06908d387f4dde0a37ccf4
Gabriel Henrique <gabrielhcm01@gmail.com>
2020-11-17 11:03:25 -0300

    Removendo complexidade do sistema, salvar dados foi implementado em adicionar.c

--- salvarEstrutura.c
+++ salvarEstrutura.c
@@ -1,18 +1,4 @@
 void salvarEstrutura(void) {
 	
-	struct tipoCarro *ptr = head;
-	
-	FILE *arquivoSaida = fopen("database.txt", "a+");
-	
-	if(arquivoSaida != NULL) {
-		
-		while(ptr != NULL) {
-			
-			
-			fwrite(&head, sizeof(struct tipoCarro),2,arquivoSaida);
-						
-		}
-		
-	}
-	fclose(arquivoSaida);
+
 }
