58e840b18c12815a4644b0f2336be1b50d097002
Gabriel Henrique <gabrielhcm01@gmail.com>
2020-12-01 20:13:30 -0300

    Adicionando funcao para carregar dados dos veiculos excluidos.

--- carregarDados.c
+++ carregarDados.c
@@ -5,4 +5,12 @@ void carregarDados(void) {
     	NUM_ID++;
 	}	
 	fclose(entradaArquivo);
+	
+	int i;
+	for(i = 0; i < NUM_ID; i++) {
+		if(nCarro[i].deletado == 1) {
+			ARRAY_DEL[DEL_ID] = nCarro[i].carroID;
+			DEL_ID++;
+		}
+	}
 }
